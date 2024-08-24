8db1a8f40b5974ca22bce6fc88f34f09f4ec7f2e
John Arby Arceo <johnarbyarceo@gmail.com>
2024-08-25 01:24:47 +0800

    ✨ implemented authentication base layout

---
+++ resources/vue/layouts/Auth.vue
@@ -0,0 +1,16 @@
+<template>
+    <q-layout view="lHh LpR fFf">
+        <q-page-container>
+            <slot />
+        </q-page-container>
+        <FooterLayout />
+    </q-layout>
+</template>
+<script>
+import FooterLayout from "../components/AuthLayout/FooterLayout.vue";
+export default {
+    components: {
+        FooterLayout,
+    },
+};
+</script>
